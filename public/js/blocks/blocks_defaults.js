1e56ca5d2b67fb4fc945eb5cc70dcb2f9f78abb7
Eunsub Kim <eunsub@gmail.com>
2015-01-05 10:50:25 +0900

    javascript variable to local

--- public/js/blocks/blocks_defaults.js
+++ public/js/blocks/blocks_defaults.js
@@ -1,3 +1,53 @@
+var variable_definitions = [];
+
+
+
+Blockly.JavaScript['variables_set'] = function(block) {
+  // Variable setter.
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+
+  if(R.contains(varName, variable_definitions)){
+    return '' + varName + ' = ' + argument0 + ';\n';
+
+  }else{
+    variable_definitions.push(varName);
+    return 'var ' + varName + ' = ' + argument0 + ';\n';
+
+
+  }
+};
+
+
+var _js_init = Blockly.JavaScript.init;
+
+Blockly.JavaScript.init = function() {
+  // Create a dictionary of definitions to be printed before the code.
+  Blockly.JavaScript.definitions_ = Object.create(null);
+  // Create a dictionary mapping desired function names in definitions_
+  // to actual function names (to avoid collisions with user functions).
+  Blockly.JavaScript.functionNames_ = Object.create(null);
+
+  if (Blockly.Variables) {
+    if (!Blockly.JavaScript.variableDB_) {
+      Blockly.JavaScript.variableDB_ =
+          new Blockly.Names(Blockly.JavaScript.RESERVED_WORDS_);
+    } else {
+      Blockly.JavaScript.variableDB_.reset();
+    }
+
+    var defvars = [];
+    var variables = Blockly.Variables.allVariables();
+    for (var x = 0; x < variables.length; x++) {
+      defvars[x] = 'xxvar ' +
+          Blockly.JavaScript.variableDB_.getName(variables[x],
+          Blockly.Variables.NAME_TYPE) + ';';
+    }
+    // Blockly.JavaScript.definitions_['variables'] = defvars.join('\n');
+  }
+};
 
 
 var ros_block_override = function(){
